f4284e33d27730e2d74a720104b86af379c6c042
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2024-06-03 17:02:35 +0600

    chore: Refactor start_avd.sh script for cross-platform compatibility

--- start_avd.sh
+++ start_avd.sh
@@ -1,7 +1,21 @@
+#@ Md Wahiduzzaman Emon (Software Engineer)
+
 #!/bin/bash
 
-# List all AVDs and filter only the lines that contain AVD names
-avd_list=$(emulator -list-avds 2>&1 | awk '/^[a-zA-Z0-9_]+$/')
+# Function to list AVDs
+list_avds() {
+    if [ "$(uname)" == "Darwin" ]; then
+        avd_list=$(emulator -list-avds 2>&1 | awk '/^[a-zA-Z0-9_]+$/')
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        avd_list=$(emulator -list-avds 2>&1 | findstr /R /C:"^[a-zA-Z0-9_]*$")
+    else
+        echo "Unsupported operating system."
+        exit 1
+    fi
+}
+
+# List all AVDs
+list_avds
 
 # Check if there are any AVDs available
 if [ -z "$avd_list" ]; then
@@ -17,7 +31,14 @@ echo "Available AVDs:"
 select avd in "${avd_array[@]}"; do
     if [ -n "$avd" ]; then
         echo "Starting AVD: $avd"
-        emulator -avd "$avd"
+        if [ "$(uname)" == "Darwin" ]; then
+            emulator -avd "$avd"
+        elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+            emulator @"$avd"
+        else
+            echo "Unsupported operating system."
+            exit 1
+        fi
         break
     else
         echo "Invalid selection. Please try again."
